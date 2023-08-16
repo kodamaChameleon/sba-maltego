eafb0ed633458b74488f88b35f86f686c0f4df49
kodamaChameleon <bryant.a.renfroe@gmail.com>
2023-08-16 13:04:28 -0400

    Update name transformation

--- transforms/PPPLoan_fromBorrowingPerson.py
+++ transforms/PPPLoan_fromBorrowingPerson.py
@@ -4,6 +4,26 @@ from maltego_trx.transform import DiscoverableTransform
 from extensions import registry,sba_set
 from modules import ckan
 
+# Broaden scope of possible name results
+def transform_name(name):
+    parts = name.split()
+    transformed_name = name
+
+    # Check if there are initials (names with more than one part)
+    if len(parts) > 2:
+        new_name_parts = []
+        for part in parts:
+            if (len(part) == 1 and part != '%') or (len(part) == 2 and part[1] == '.'):
+                new_name_parts.append(part[0] + '%')
+            else:
+                new_name_parts.append(part)
+        transformed_name = ' '.join(new_name_parts)
+
+    elif len(parts) == 2:
+        transformed_name = f'{parts[0]}% {parts[1]}'
+
+    return transformed_name
+
 @registry.register_transform(
     display_name="Get PPP Loans [SBA]", 
     input_entity="maltego.Person",
@@ -19,16 +39,9 @@ class PPPLoan_fromBorrowingPerson(DiscoverableTransform):
 
         async def main():
 
-            full_name = request.Value
-
-            # If firstname lastname, add % for missing middle names/initials
-            name_part = full_name.split(' ')
-            if len(name_part) == 2:
-                full_name = name_part[0] + " % " + name_part[1]
-
             data = ckan.dataset("ppp-foia")
             search_terms = {
-                "BorrowerName": full_name
+                "BorrowerName": transform_name(request.Value)
             }
 
             results = data.get_rows(search_terms)
